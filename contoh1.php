12aefc7869d8d0cd88a396cc575cd58a79121526
aldimrd <amardian532@gmail.com>
2021-09-24 15:55:07 +0700

    menambahkan contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>;
+        echo "Nama saya Aldi Mardian
+            Saya tinggal di Jalan Gunung Subang
+            Musik yang saya sukai adalah
+            Dangdut
+            Nomor handphone 088802165368";
+    }
+}
+?>
